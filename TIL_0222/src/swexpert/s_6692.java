3d4b5fdacc5a0ff923f5367e8577362ed0297b86
seongyun-jeong <seongyun-jeong@localhost>
2020-02-22 20:38:14 +0900

    swexpert_0222

---
+++ TIL_0222/src/swexpert/s_6692.java
@@ -0,0 +1,21 @@
+package swexpert;
+
+import java.util.*;
+
+public class s_6692 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int tc = 1; tc <= T; tc++) {
+			int n = sc.nextInt();
+			double sum = 0.0;
+			for(int i=0; i<n; i++) {
+				double p = sc.nextDouble();
+				double x = sc.nextDouble();
+				sum += p*x;
+			}
+			System.out.println("#"+tc+" "+sum);
+		}
+	}
+}
+
