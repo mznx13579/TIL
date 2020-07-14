6452b5df3f1b9d00c5bdb509083765917fcb51d7
정성윤 <mznx13579@icloud.com>
2020-07-14 23:21:46 +0900

    TIL_0714_done

--- src/TIL_0714/Main_bj_11053_가장긴증가하는부분수열.java
+++ src/TIL_0714/Main_bj_11053_가장긴증가하는부분수열.java
@@ -1,5 +1,26 @@
 package TIL_0714;
 
+import java.util.*;
 public class Main_bj_11053_가장긴증가하는부분수열 {
-
+	static int N;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		int[] arr = new int[N];
+		for(int i=0; i<N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int[] ans = new int[N];
+		Arrays.fill(ans, 1);
+		int max = 0;
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<i; j++) {
+				if(arr[i] > arr[j] && ans[j]+1 > ans[i]) ans[i] = ans[j]+1;
+			}
+		}
+		for(int i=0; i<N; i++) {
+			if(ans[i] > max) max = ans[i];
+		}
+		System.out.println(max);
+	}
 }
