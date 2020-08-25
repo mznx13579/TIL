7141e040fde8708402d2fe38596d5a2396382bb6
정성윤 <mznx13579@icloud.com>
2020-08-26 00:10:01 +0900

    TIL_0826_sunset

--- src/TIL_0826/Main_bj_9095_123더하기.java
+++ src/TIL_0826/Main_bj_9095_123더하기.java
@@ -1,5 +1,27 @@
 package TIL_0826;
 
-public class Main_bj_9095_123더하기 {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main_bj_9095_123더하기 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int T = Integer.parseInt(st.nextToken());
+		int[] ans = new int[12];
+		ans[1] = 1;
+		ans[2] = 2;
+		ans[3] = 4;
+		for(int i=4; i<12; i++) {
+			ans[i] = ans[i-1]+ans[i-2]+ans[i-3];
+		}
+		StringBuilder sb = new StringBuilder();
+		for(int tc = 1; tc <= T; tc++) {
+			st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			sb.append(ans[n]).append('\n');
+		}
+		System.out.println(sb);
+	}
 }
