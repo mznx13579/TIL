6e8a5dedbcf84b9ff5ac82cdc24e6cd427efc943
정성윤 <mznx13579@icloud.com>
2020-09-10 21:26:55 +0900

    TIL_0910

---
+++ src/TIL_0910/KAKAO_practice1.java
@@ -0,0 +1,13 @@
+package TIL_0910;
+import java.util.*;
+
+public class KAKAO_practice1 {
+	public static void main(String[] args) {
+		String s = "aabbaccc";
+		int answer = s.length();
+		int limit = s.length()/2;
+		for(int i=1; i<=limit; i++) {
+			
+		}
+	}
+}
