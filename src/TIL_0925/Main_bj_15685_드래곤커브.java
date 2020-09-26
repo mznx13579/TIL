6c8cc974439153dfbf3feaba49e4e592112d0341
정성윤 <mznx13579@icloud.com>
2020-09-26 22:43:56 +0900

    TIL_0926

---
+++ src/TIL_0925/Main_bj_15685_드래곤커브.java
@@ -0,0 +1,13 @@
+package TIL_0925;
+import java.util.*;
+
+public class Main_bj_15685_드래곤커브 {
+	static int N, x, y, d, g;
+	static int[] dr = {0,-1,0,1};
+	static int[] dc = {1,0,-1,0};
+	static int[][] map = new int[101][101];
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+	}
+}
